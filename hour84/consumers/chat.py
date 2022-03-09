38196310d27ae34ed9c4ccb9967fc10ddf970f74
poker <wanglei311600@qq.com>
2022-03-09 06:56:04 +0000

    modify chat consumer

--- hour84/consumers/chat.py
+++ hour84/consumers/chat.py
@@ -52,42 +52,49 @@ class Chat(WebsocketConsumer):
                     }
             )
     
-    def user_login_or_register(self, username='',password=None):
+    
+    def user_login_or_register(self, username,password,setting): # entered the password
         print("user_login_or_register")
         resp = {
                 'action':'login',
                 'status':True,
             }
+        self.from_db = True
         print(username, '-', password)
         user = myUser.objects.filter(username=username)
         if len(user): # username has occured
             user = myUser.objects.filter(username=username, password=password)
-            if len(user): # regular user login success
-                pass
-            elif password: # password is not correct
+            if not len(user):
                 resp['status']=False
                 resp['reason']='password is not correct'
-            else: # dont input a passwod and username is duplicated
-                resp['action']= 'register'
-                resp['status']= False
-                resp['reason']='depulicate username'
-        elif password: # user register
-            resp['action'] = 'register'
-            myUser.objects.create(username=username, password=password)
-        if resp['status']:
-            self.add_user_online(username)
-            if resp['action'] == 'login':
-                self.load_user_from_db()
+        else:
+            resp['action']='register'
+            myUser.objects.create(username=username,password=password,setting=setting)
+        if username not in ONLINE_USER and  resp['status']:
+            self.add_user_online()
         self.send(json.dumps(resp))
 
-    def anonymous_user_login(self):
+    def anonymous_user_login(self,username): # didnt enter the password
         print("anonymous_user_login")
+        user  = myUser.objects.filter(username=username)
+        resp = {
+                'action':'anonymous_login',
+                'status':True
+            }
+        if len(user):
+            resp['status']=False
+            resp['reason']='username duplicated'
+        if username not in ONLINE_USER and resp['status']:
+            self.add_user_online(username)
+        self.send(json.dumps(resp))
 
     def add_user_online(self,username):
         self.username=username
         ONLINE_USER.add(self.username)
         cache.set('friend_%s'%self.username,[],None)
         cache.set('group_%s'%self.username,[],None)
+        if self.from_db:
+            self.load_user_from_db()
 
     def update_user_in_db(self,username):
         pass
@@ -95,13 +102,21 @@ class Chat(WebsocketConsumer):
     def load_user_from_db(self):
         user = myUser.objects.get(username=self.username)
         friends = user.friends.all()
-        print(friends)
+        friend_list = []
+        for it in friends:
+            friend_list.append(it.username)
+        cache.set('friend_%s'%self.username,friend_list)
+        print(friend_list)
+        print(cache.get('friend_%s'%self.username))
 
     def remove_user_offline(self):
         cache.delete_many(['friend_%s'%self.username,'group_%s'%self.username])
 
     def login_event(self, data):
-        self.user_login_or_register(data['username'], data['password'])
+        if data['password']:
+            self.user_login_or_register(data['username'], data['password'],data['setting'])
+        else:
+            self.anonymous_user_login(data['username'])
 
     def search_event(self, data):
         print("search ....")
